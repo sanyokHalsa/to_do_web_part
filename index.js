39e1b1bd93055342b7f1947d1884c858cb6734fb
sanyokHalsa <126600330+sanyokHalsa@users.noreply.github.com>
2023-05-16 19:50:56 +0300

    sorting users

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/register', async (req, res) => {
 //======================================= GET
 app.get('/leaders', async (req, res) => {
     let resp_arr = [];
-    let search_res = await User.find({}).limit(10);
+    let search_res = await User.find({}).sort({ points: -1 }).limit(10);
     for(let i = 0; i<search_res.length;i++){
         search_res[i].username
         resp_arr.push({username:search_res[i].username, tasks:search_res[i].completed_tasks});
